a43e340a2cdb5a941628292d14c1ce425fcb4a59
Matt Oliver <protogonoi@gmail.com>
2019-06-05 01:40:52 +1000

    TestStream: Update to handle files with small durations.

--- test/FFFRTestStream.cpp
+++ test/FFFRTestStream.cpp
@@ -73,7 +73,9 @@ TEST_P(StreamTest1, getFrameRate)
 
 TEST_P(StreamTest1, seek)
 {
-    const double timeStamp1 = (static_cast<double>(80) * (1000000.0 / GetParam().m_frameRate));
+    constexpr uint32_t seekFrames = 80;
+    const uint32_t actualSeek = seekFrames >= GetParam().m_totalFrames ? GetParam().m_totalFrames - 5 : seekFrames;
+    const double timeStamp1 = (static_cast<double>(actualSeek) * (1000000.0 / GetParam().m_frameRate));
     const auto time1 = llround(timeStamp1);
     ASSERT_TRUE(m_stream->seek(time1));
     const auto ret1 = m_stream->getNextFrame();
@@ -100,7 +102,7 @@ TEST_P(StreamTest1, seekFail)
 {
     ASSERT_FALSE(m_stream->seek(m_stream->getDuration()));
     ASSERT_FALSE(m_stream->seek(m_stream->getDuration() + 300000));
-    //Check we can do a valid seek after a failed one
+    // Check we can do a valid seek after a failed one
     const double timeStamp1 = (static_cast<double>(2) * (1000000.0 / GetParam().m_frameRate));
     const auto time1 = llround(timeStamp1);
     ASSERT_TRUE(m_stream->seek(time1));
@@ -120,30 +122,43 @@ TEST_P(StreamTest1, seekEnd)
 
 TEST_P(StreamTest1, seekLoop)
 {
-    double timeStamp1 = 0.0;
-    int64_t time1 = 0;
-    // Perform multiple forward seeks
-    for (uint32_t i = 0; i < 5; i++) {
-        ASSERT_TRUE(m_stream->seek(time1));
-        // Check that multiple sequential frames can be read
-        int64_t time2 = time1;
-        for (uint32_t j = 0; j < 25; j++) {
-            const auto ret1 = m_stream->getNextFrame();
-            ASSERT_NE(ret1.index(), 0);
-            const auto frame1 = std::get<1>(ret1);
-            ASSERT_EQ(frame1->getTimeStamp(), time2);
-            const double timeStamp2 = timeStamp1 + (static_cast<double>(j + 1) * (1000000.0 / GetParam().m_frameRate));
-            time2 = llround(timeStamp2);
+    constexpr uint32_t seekJump = 40;
+    constexpr uint32_t numLoops = 5;
+    constexpr uint32_t numFrames = 25;
+    if (GetParam().m_totalFrames >= numLoops * (seekJump + numFrames)) {
+        double timeStamp1 = 0.0;
+        int64_t time1 = 0;
+        // Perform multiple forward seeks
+        for (uint32_t i = 0; i < numLoops; i++) {
+            ASSERT_TRUE(m_stream->seek(time1));
+            // Check that multiple sequential frames can be read
+            int64_t time2 = time1;
+            for (uint32_t j = 0; j < numFrames; j++) {
+                const auto ret1 = m_stream->getNextFrame();
+                // Allow EOF
+                if (ret1.index() == 0 && std::get<0>(ret1) == true) {
+                    return;
+                }
+                ASSERT_NE(ret1.index(), 0);
+                const auto frame1 = std::get<1>(ret1);
+                ASSERT_EQ(frame1->getTimeStamp(), time2);
+                const double timeStamp2 =
+                    timeStamp1 + (static_cast<double>(j + 1) * (1000000.0 / GetParam().m_frameRate));
+                time2 = llround(timeStamp2);
+            }
+            timeStamp1 =
+                (static_cast<double>(i + 1) * static_cast<double>(seekJump) * (1000000.0 / GetParam().m_frameRate));
+            time1 = llround(timeStamp1);
         }
-        timeStamp1 = (static_cast<double>(i + 1) * 40.0 * (1000000.0 / GetParam().m_frameRate));
-        time1 = llround(timeStamp1);
     }
 }
 
 TEST_P(StreamTest1, seekBack)
 {
     // Seek forward
-    const double timeStamp1 = (static_cast<double>(80) * (1000000.0 / GetParam().m_frameRate));
+    constexpr uint32_t seekFrames = 80;
+    const uint32_t actualSeek = seekFrames >= GetParam().m_totalFrames ? GetParam().m_totalFrames - 5 : seekFrames;
+    const double timeStamp1 = (static_cast<double>(actualSeek) * (1000000.0 / GetParam().m_frameRate));
     const auto time1 = llround(timeStamp1);
     ASSERT_TRUE(m_stream->seek(time1));
     auto ret1 = m_stream->getNextFrame();
@@ -160,28 +175,39 @@ TEST_P(StreamTest1, seekBack)
 
 TEST_P(StreamTest1, seekFrameLoop)
 {
-    int64_t frame = 0;
-    // Perform multiple forward seeks
-    for (uint32_t i = 0; i < 5; i++) {
-        ASSERT_TRUE(m_stream->seekFrame(frame));
-        // Check that multiple sequential frames can be read
-        int64_t frame2 = frame;
-        for (uint32_t j = 0; j < 25; j++) {
-            const auto ret1 = m_stream->getNextFrame();
-            ASSERT_NE(ret1.index(), 0);
-            const auto frame1 = std::get<1>(ret1);
-            ASSERT_EQ(frame1->getFrameNumber(), frame2);
-            ++frame2;
+    constexpr uint32_t seekJump = 40;
+    constexpr uint32_t numLoops = 5;
+    constexpr uint32_t numFrames = 25;
+    if (GetParam().m_totalFrames >= numLoops * (seekJump + numFrames)) {
+        int64_t frame = 0;
+        // Perform multiple forward seeks
+        for (uint32_t i = 0; i < numLoops; i++) {
+            ASSERT_TRUE(m_stream->seekFrame(frame));
+            // Check that multiple sequential frames can be read
+            int64_t frame2 = frame;
+            for (uint32_t j = 0; j < numFrames; j++) {
+                const auto ret1 = m_stream->getNextFrame();
+                // Allow EOF
+                if (ret1.index() == 0 && std::get<0>(ret1) == true) {
+                    return;
+                }
+                ASSERT_NE(ret1.index(), 0);
+                const auto frame1 = std::get<1>(ret1);
+                ASSERT_EQ(frame1->getFrameNumber(), frame2);
+                ++frame2;
+            }
+            frame += seekJump;
         }
-        frame += 40;
     }
 }
 
 TEST_P(StreamTest1, getNextFrameSequenceSeek)
 {
     // First seek to a frame
-    ASSERT_TRUE(m_stream->seekFrame(80));
-    // Now get frame sequence off set from current
+    constexpr uint32_t seekFrames = 80;
+    const uint32_t actualSeek = seekFrames >= GetParam().m_totalFrames ? GetParam().m_totalFrames - 9 : seekFrames;
+    ASSERT_TRUE(m_stream->seekFrame(actualSeek));
+    // Now get frame sequence offset from current
     const std::vector<int64_t> framesList1 = {0, 1, 5, 7, 8};
     const auto ret1 = m_stream->getNextFrameSequence(framesList1);
     ASSERT_NE(ret1.index(), 0);
@@ -189,7 +215,7 @@ TEST_P(StreamTest1, getNextFrameSequenceSeek)
     const auto frames1 = std::get<1>(ret1);
     auto j = 0;
     for (auto& i : frames1) {
-        ASSERT_EQ(i->getFrameNumber(), framesList1[j] + 80);
+        ASSERT_EQ(i->getFrameNumber(), framesList1[j] + actualSeek);
         ++j;
     }
 }
