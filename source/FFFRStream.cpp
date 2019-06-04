ba50a31046632bfa8c2c92f38e0f20cc4c9cd557
Aiden Nibali <dismaldenizen@gmail.com>
2019-06-05 08:49:10 +1000

    Fix some compilation errors

--- source/FFFRStream.cpp
+++ source/FFFRStream.cpp
@@ -668,7 +668,8 @@ bool Stream::decodeNextFrames() noexcept
         }
 
         // Add the new frame to the pong buffer
-        m_bufferPong.emplace_back(make_shared<Frame>(move(frame), timeStamp, frameNum, thisPtr));
+        auto moved_frame = move(frame);
+        m_bufferPong.emplace_back(make_shared<Frame>(moved_frame, timeStamp, frameNum, thisPtr));
         frame = Frame::FramePtr(av_frame_alloc());
     }
 }
